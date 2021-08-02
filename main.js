b31908e26f5208be08aed7d487341ff64bbc4b70
Apurbu Sarder <apurbusarderjust321@gmail.com>
2021-08-02 20:59:44 +0600

    Finished

--- main.js
+++ main.js
@@ -1,72 +1,72 @@
 // Animation on scroll
 AOS.init({
-    duration: 800,
-    easing: "slide",
-  });
-  
-  (function ($) {
-    "use strict";
-    $(window).stellar({
-      responsive: true,
-      parallaxBackgrounds: true,
-      parallaxElements: true,
-      horizontalScrolling: false,
-      hideDistantElements: false,
-      scrollProperty: "scroll",
-    });
-  
-    //   full height
-    var fullHeight = function () {
-      $(".js-fullheight").css("height", $(window).height());
-      $(window).resize(function () {
-        $(".js-fullheight").css("height", $(window).height());
-      });
-    };
-    fullHeight(); //call that function
-  
-    // navbar scroll
-    var scrollWindow = function () {
-      $(window).scroll(function () {
-        var $w = $(this),
-          st = $w.scrollTop(),
-          navbar = $(".ftco_navbar"),
-          sd = $(".js-scroll-wrap");
-        if (st > 150) {
-          if (!navbar.hasClass("scrolled")) {
-            navbar.addClass("scrolled");
-          }
-        }
-        if (st < 150) {
-          if (navbar.hasClass("scrolled")) {
-            navbar.removeClass("scrolled sleep");
-          }
-        }
-  
-        if (st > 350) {
-          if (!navbar.hasClass("awake")) {
-            navbar.addClass("awake");
-          }
-          if (sd.length > 0) {
-            sd.addClass("sleep");
-          }
-        }
-  
-        if (st < 350) {
-          if (navbar.hasClass("awake")) {
-            navbar.removeClass("awake");
-            navbar.addClass("sleep");
-          }
-          if (sd.length > 0) {
-            sd.removeClass("sleep");
-          }
-        }
-      });
-    };
-    scrollWindow();
-  
-    $.Scrollax();
+  duration: 800,
+  easing: "slide",
+});
 
-    //   carousel
+(function ($) {
+  "use strict";
+  $(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: "scroll",
+  });
+
+  //   full height
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
+  };
+  fullHeight(); //call that function
+
+  // navbar scroll
+  var scrollWindow = function () {
+    $(window).scroll(function () {
+      var $w = $(this),
+        st = $w.scrollTop(),
+        navbar = $(".ftco_navbar"),
+        sd = $(".js-scroll-wrap");
+      if (st > 150) {
+        if (!navbar.hasClass("scrolled")) {
+          navbar.addClass("scrolled");
+        }
+      }
+      if (st < 150) {
+        if (navbar.hasClass("scrolled")) {
+          navbar.removeClass("scrolled sleep");
+        }
+      }
+
+      if (st > 350) {
+        if (!navbar.hasClass("awake")) {
+          navbar.addClass("awake");
+        }
+        if (sd.length > 0) {
+          sd.addClass("sleep");
+        }
+      }
+
+      if (st < 350) {
+        if (navbar.hasClass("awake")) {
+          navbar.removeClass("awake");
+          navbar.addClass("sleep");
+        }
+        if (sd.length > 0) {
+          sd.removeClass("sleep");
+        }
+      }
+    });
+  };
+  scrollWindow();
+
+  $.Scrollax();
+
+  //   carousel
   var carousel = function () {
     $(".home-slider").owlCarousel({
       loop: true,
@@ -194,4 +194,4 @@ AOS.init({
     autoclose: true,
   });
   $("#book_time").timepicker();
-  })(jQuery);
+})(jQuery);
